f2ba10e6887b49889a3d10f7d4a4c286e57e9326
Wolfur1 <79707790+Wolfur1@users.noreply.github.com>
2023-03-31 18:31:53 +0200

    Add files via upload

---
+++ info/script.js
@@ -0,0 +1,15 @@
+function DiscordBotButton(){
+	location.href = '#DiscordBot';
+}
+function ScoritoButton(){
+	location.href = '#Scorito';
+}
+function SlapperButton(){
+	location.href = '#Slapper';
+}
+function StreamerGroupButton(){
+	location.href = '#StreamerGroup';
+}
+function TwtichButton(){
+	location.href = '#Twitch';
+}
